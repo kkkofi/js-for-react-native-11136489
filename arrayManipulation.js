5d5f41faff3e42ca5fd565aadb857a393089b461
kddjan <joelkddjan@gmail.com>
2024-05-18 20:07:40 +0000

    task two done

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -15,3 +15,25 @@ function processArray(arr) {
      processArray(inputArray);
     console.log(outputArray); 
 
+    function formatArrayStrings(Strings, Numbers) {
+        const formattedStrings = [];
+        for (let i = 0; i < Strings.length; i++) {
+          const string = Strings[i];
+          const number = Numbers[i];
+          if (number % 2 === 0) {
+            formattedStrings.push(string.toUpperCase());
+          } else {
+            formattedStrings.push(string.toLowerCase());
+          }
+        }
+        return formattedStrings;
+      }
+      
+    
+    const originalStrings = ["Huge”,  “Strong”, “Small”, “weak"];
+    const processedNumbers = [18, 33, 26, 44]; 
+    
+    const formattedStrings = formatArrayStrings(originalStrings, processedNumbers);
+    
+    console.log("Original Strings:", originalStrings);
+    console.log("Formatted Strings:", formattedStrings);
